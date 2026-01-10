b5522ce8f9954307429584e053b515f1914066ed
Omar <omar.assed@gmail.com>
2026-01-10 07:38:22 -0700

    Added category id management, delete category api

--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -9,6 +9,8 @@ import java.util.List;
 @Service
 public class CategoryServiceImpl implements CategoryService {
     private List<Category> categories = new ArrayList<>();
+    private Long nextId = 1L;
+
     @Override
     public List<Category> getAllCategories() {
         return categories;
@@ -16,6 +18,19 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public void createCategory(Category category) {
+        category.setCategoryId(nextId++);
         categories.add(category);
+    }
+
+    @Override
+    public String deleteCategory(Long categoryId) {
+        Category category = categories.stream()
+                .filter(c -> c.getCategoryId().equals(categoryId))
+                .findFirst().orElse(null);
+        if (category == null) {
+            return "Category not found";
+        }
+        categories.remove(category);
+        return "Category with categoryId: "+ categoryId+ " deleted successfully";
     }
 }
